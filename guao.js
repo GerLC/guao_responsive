3e3e86ab07feba1f557ab9b02993ac5065ff6527
devendr4 <angel.emo.altuve@gmail.com>
2020-03-12 10:32:30 -0400

    FIX

--- guao.js
+++ guao.js
@@ -4,25 +4,19 @@ meta.setAttribute("content", "width=device-width, initial-scale=1.0")
 head = document.getElementsByTagName("head")[0]
 head.append(meta)
 
-jQuery(document).ready(function(){
-  console.log("jQuery disponible.");
+jQuery(document).ready(function() {
+    console.log("jQuery disponible.");
 });
 
 function myFunction(y) {
-  var x = document.getElementById("main-menu-links");
-  
-  if (x.style.display === "none") {
-    x.style.display = "block";
-    
-    y.classList.toggle("change");
-  } else {
-    x.style.display = "none";
-    y.classList.toggle("change");
-  }
-}
+    var x = document.getElementById("main-menu-links");
 
- <div class="hamburger" onclick="myFunction(this)">
-  <div class="bar1"></div>
-  <div class="bar2"></div>
-  <div class="bar3"></div>
-</div>
+    if (x.style.display === "none") {
+        x.style.display = "block";
+
+        y.classList.toggle("change");
+    } else {
+        x.style.display = "none";
+        y.classList.toggle("change");
+    }
+}
--- guao_responsive_design.js
+++ guao_responsive_design.js
@@ -4,6 +4,6 @@ meta.setAttribute("content", "width=device-width, initial-scale=1.0")
 head = document.getElementsByTagName("head")[0]
 head.append(meta)
 
-jQuery(document).ready(function(){
-  console.log("jQuery disponible.");
+jQuery(document).ready(function() {
+    console.log("jQuery disponible.");
 });
