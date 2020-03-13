79b3576b9d44e380a09d7aa099b282ddf83f49a3
GerLC <gli.16@est.ucab.edu.ve>
2020-03-13 15:25:48 -0400

    menu1

--- guao.js
+++ guao.js
@@ -4,19 +4,26 @@ meta.setAttribute("content", "width=device-width, initial-scale=1.0")
 head = document.getElementsByTagName("head")[0]
 head.append(meta)
 
-jQuery(document).ready(function() {
-    console.log("jQuery disponible.");
+jQuery(document).ready(function(){
+  console.log("jQuery disponible.");
 });
 
-function myFunction(y) {
-    var x = document.getElementById("main-menu-links");
+var acc = document.getElementsByClassName("menu-437");
+var i;
 
-    if (x.style.display === "none") {
-        x.style.display = "block";
-
-        y.classList.toggle("change");
-    } else {
-        x.style.display = "none";
-        y.classList.toggle("change");
-    }
+for (i = 0; i < acc.length; i++) {
+ acc[i].addEventListener("click", function() {
+ this.classList.toggle("active");
+var panel = this.nextElementSibling;
+ if (panel.style.maxHeight) {
+ panel.style.maxHeight = null;
+ } else {
+ panel.style.maxHeight = panel.scrollHeight + "px";
+} 
+ });
 }
+
+$('.menu-437').click(function(e) {
+ e.preventDefault();
+//do other stuff when a click happens
+});
